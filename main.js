e9b6d5b4ba5823acc16db4588dd15a06d309cd37
diego <diegozaratean@gmail.com>
2025-12-15 20:48:49 +0000

    finish js intro class

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log('desde main js')
+
+let nombre = 'bugs bunny'
+let edad = 80
+const fechaNacimeinto = "1945"
+let esta_vivo = true
+
+
+// elementos.   1.       2.       3.          4.       5.    
+let frutas = ['lulo','pitaya','guanabana','Durian','tomate de arbol']
+// index/pos.    0.      1.        2.        3.       4
+// longitud 5
+
+let perro = {nombre: 'eeros',color: 'beige', size: 'grande',raza: 'golden retriever'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Super heroe
+// poderes, uniforme, nombre,identidad
+
+let heroe = {poderes: ["sentido aracnido", "escalar"],nombre: 'spiderman',uniforme: 'rojo y azul',identidad:"peter parker"}
