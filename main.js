1f10b9265c06d3e7244cd9261fc955a851438d61
diego <diegozaratean@gmail.com>
2025-12-17 23:25:38 +0000

    finish function js class

--- main.js
+++ main.js
@@ -1,118 +1,130 @@
 
-console.log(1)
-console.log(2)
-console.log(3)
-console.log(4)
-console.log(5)
-
-// for(Inicialización;Condición;Actualización){}
-
-/// i = 20   20 >= 15   ===> 20
-/// i = 20   20 >= 15   ===> 20
-/// i = 20   20 >= 15   ===> 20
-/// i = 20   20 >= 15   ===> 20
-/// i = 16   16 >= 15   ===> 16
-/// i = 15   15 >= 15   ===> 15
-/// i = 14   14 >= 15   ===> 
-/// i = 2   2 <= 5   ===> 2
-/// i = 3   3 <= 5   ===> 3
-/// i = 4   4 <= 5   ===> 4
-/// i = 5   5 <= 5   ===> 5
-/// i = 6   6 <= 5   ===> 
 
 
-/// i++ ====> i = i + 1
-/// i-- ====> i = i - 1    
+// let name = 'Diego'
+// console.log('Andes de la funcion ' +name)
+// function saludarAlumno(name){
+//     console.log('Dentro de la funcion ' +name)
+//     console.log('Hola ' + name)
+// }
+// console.log('Despues de la funcion ' +name)
+// saludarAlumno('Rodolfo')
 
-for( i = 20; i >= 15; i--){
-    console.log(i)
+
+
+// saludarAlumno('Morena')
+// saludarAlumno('Juan')
+
+// function transportar(origen,destino){
+//     console.log('Saliendo de ' + origen)
+//     console.log('moviendome')
+//     console.log('Llegando a ' + destino)
+// }
+
+
+// transportar("Casa","Gym")
+// transportar("Gym","oficina")
+// transportar("oficina","Casa")
+
+// transportar()
+// transportar()
+
+
+// hacer una fucnion que sume dos numeros
+// function sumar(numa,numb){
+//     return numa+numb
+// }
+
+// console.log(sumar(3,4))
+
+// hacer una funcion adminitido en el bar si es menor de edad "Debes esperar" si es mayor "Si puedes entrar"
+// function admitido(edad){
+//     if(edad >= 18){
+//         return "Si puedes entrar"
+//     }else{
+//         return "Debes esperar"
+
+//     }
+// }
+
+// console.log(admitido(2))
+
+
+function calcularImpuesto(ganancia, tax){
+    return ganancia *(tax/100)
 }
 
 
 
+document.getElementById('tax').innerHTML = calcularImpuesto(10000,30)
+// console.log(calcularImpuesto(10000,5))
+// console.log(calcularImpuesto(10000,20))
 
 
 
 
 
-// i = 30 30<=50 ===> 30
-// i = 31 31 <= 50 ===> 31
 
 
 
 
 
-console.log("OPC A")
-for(i=30; i <= 50 ; i++){
-    if(i % 5 == 0){
-        console.log(i)
-        // console.log('IMPRIMIR')
 
-    }
+
+
+
+
+
+
+// Crear una funcion de calculadora
+// function sumar(numa,numb){
+//    return numa + numb
+// }
+
+//  console.log(sumar(2,2)) // 4
+//  console.log(sumar(11,13)) // 24
+//  console.log(sumar(5,0)) // 5
+//  console.log(sumar(6,4)) // 10
+
+
+// console.log("ANTES se ejecuto window onload")
+// window.onload = function (){
+//     console.log("DENTRO se ejecuto window onload")
+// }
+// console.log("DESPUES se ejecuto window onload")
+
+
+// Funcion declarativa
+saludoDeclarativo()
+function saludoDeclarativo(){
+    console.log('saludoDeclarativo')
 }
-// i = i +5 ====> i+=5
-console.log("OPC B")
-for(i=30; i<=50; i+=5){
-    console.log(i)
+
+// Funcion de Expresión
+const saludoExpresion = function(){
+    console.log('saludoExpresion')
 }
+saludoExpresion()
 
-
-
-
-//.   elm         1         2            3          4 
-let nombres = ['goku','walter white','messi','neymar','guanbana','lulo']
-// pos.           0         1           2           3
-// 4
-
-console.log(nombres)
-nombres.push('Crash')
-console.log(nombres)
-
-const alumnos = ['daniegl','leonardo', 'humberto']
-console.log(alumnos)
-alumnos.push('rodolfo')
-console.log(alumnos)
-
-
-const frutas = ['kiwi','Uchuva','Piña','Naranja','guanbana','lulo']
-
-console.log(frutas)
-
-console.log(frutas[2])
-
-frutas.push('manzana')
-console.log(frutas)
-
-frutas.unshift('banana')
-console.log(frutas)
-
-frutas.pop()
-console.log(frutas)
-
-frutas.shift()
-console.log(frutas)
-
-frutas.splice(2,1)
-console.log(frutas)
-
-frutas.splice(2,0,'fresa')
-console.log(frutas)
-
-
-frutas.splice(2,1,'pera')
-console.log(frutas)
-
-
-console.log('QUEMADO')
-console.log(frutas[0] + 's')
-console.log(frutas[1] + 's')
-console.log(frutas[2] + 's')
-console.log(frutas[3] + 's')
-
-
-console.log('DINAMICO')
-console.log(frutas)
-for(let i = frutas.length - 1 ; i  >= 0 ;i--){
-    // console.log(i)
-    console.log(frutas[i] + 's')
+// Funcion flecha arrow
+const saludoArrow = ()=>{
+    console.log('saludoArrow')
+    // this
 }
+saludoArrow()
+
+
+///// REFACTORING //////////////////////////////////////////////////////////
+
+
+//// PSEUDOCODIGO
+// desarrollar un generador de excusas para Quien,Accion ,Cuando y Que
+// 1. Modificar html desde js
+// 2. Elegir un elemento aleatorio del array Quien 
+// 3. Elegir un elemento aleatorio del array Accion
+// 4. Elegir un elemento aleatorio del array Cuando
+// 5. Elegir un elemento aleatorio del array Que
+// 6. Aramar una frase con los elementos aleatoris
+// 7. agregar estilos
+// 8. traer un elemento de hmtl a js
+// 9. modificar el contenido d eun un elemento hmtl desde js
